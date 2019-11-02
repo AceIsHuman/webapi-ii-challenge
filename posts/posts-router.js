79a0972c99a682203dcff7a221a3ac6f342e82d3
AceIsHuman <menoman1995@yahoo.com>
2019-11-02 16:19:16 -0500

    create post for comments

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const db = require("../data/db");
 
+// Add a Post
+
 router.post("/", (req, res) => {
   const { title, contents } = req.body;
   if (!title || !contents) {
@@ -15,11 +17,53 @@ router.post("/", (req, res) => {
         if (post) {
           return res.status(201).json(post);
         }
-        return res.status(500).json({ error: "There was an error while saving the post to the database" });
+        return res
+          .status(500)
+          .json({
+            error: "There was an error while saving the post to the database"
+          });
       });
     })
     .catch(err => {
-      return res.status(500).json({ error: "There was an error while saving the post to the database" });
+      return res
+        .status(500)
+        .json({
+          error: "There was an error while saving the post to the database"
+        });
+    });
+});
+
+// Add a comment to a post
+
+router.post("/:id/comments", (req, res) => {
+  const id = req.params.id;
+  const { text } = req.body;
+  if (!text) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide text for the comment." });
+  }
+  db.insertComment({ ...req.body, post_id: id })
+    .then(({ id }) => {
+      db.findCommentById(id)
+        .then(comment => {
+          return res.status(201).json(comment);
+        })
+        .catch(err => {
+          return res
+            .status(500)
+            .json({
+              error:
+                "There was an error while saving the comment to the database"
+            });
+        });
+    })
+    .catch(err => {
+      return res
+        .status(404)
+        .json({
+          message: `The post with the specified ID, ${id}, does not exist.`
+        });
     });
 });
 
