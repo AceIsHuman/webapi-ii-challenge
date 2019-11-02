af5e108e87ca33d3f2e0ac889146635349bfe543
AceIsHuman <menoman1995@yahoo.com>
2019-11-02 18:45:28 -0500

    create put request for posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -125,6 +125,8 @@ router.get("/:id/comments", (req, res) => {
     });
 });
 
+// Delete a post
+
 router.delete("/:id", (req, res) => {
   const id = req.params.id;
   db.remove(id)
@@ -140,6 +142,32 @@ router.delete("/:id", (req, res) => {
     .catch(err => {
       return res.status(500).json({ error: "The post could not be removed." });
     });
+});
+
+// Update a post
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const {title, contents} = req.body;
+  if (!title, !contents) {
+    return res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+  }
+  db.update(id, req.body)
+    .then(count => {
+      if (count === 0) {
+        return res.status(404).json({ message: "The post with the specified ID does not exist." });
+      }
+      db.findById(id)
+        .then(post => {
+          return res.status(200).json(post)
+        })
+        .catch(err => {
+          return res.status(500).json({ error: "The updated post information could not be retrived."});
+        })
+    })
+    .catch(err => {
+      return res.status(500).json({ error: "The post information could not be modified." });
+    })
 });
 
 module.exports = router;
